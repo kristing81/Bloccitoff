3bfceb8f8f57e58b877b8229da7d6a538a4896ec
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-08-25 16:15:00 +0800

    Working through testing user sign in flow

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
 
-feature 'New user signs in' do
-  scenario 'Successfully' do
+feature 'User signs in and out' do
+  scenario 'Successfully sign in' do
     visit user_session_path
-    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Email', with: 'user.email'
     fill_in 'Password', with: 'password'
-    click_button 'Sign In'
-    expect(page).to have_content('Welcome back #{user.name}')
-    expect(page).to have_content('Welcome to Bloccitoff')
+    click_button 'Sign in'
+    expect(current_path).to eq user_session_path
+    expect(page).to have_content('Hello #{user.email}!')
+    expect(page).to have_content('Signed in successfully')
+  end
+  scenario 'Successfully sign out' do
+    click_button 'Sign out'
+    expect(current_path).to eq destroy_user_session_path
+    expect(page).to have_content('Signed out successfully')
   end
 end
+
+
+#As a user, I should be able to sign in and out of Blocitoff.
+
+# Visit sign in page
+# enter information
+# Successfully sign in
+# click Sign out
+# Successfully sign out
